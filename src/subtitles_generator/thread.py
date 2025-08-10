a653ef19bc83b3ee8fff4b09af6de2899b3ce387
Tiago Ferreira <tiagoferreira@Tiagos-MBP.home>
2025-08-10 19:22:24 +0100

    Adding the possibility to either select if you want parallel or not

--- src/subtitles_generator/thread.py
+++ src/subtitles_generator/thread.py
@@ -3,20 +3,21 @@ from watchdog.events import FileSystemEventHandler
 from manipulation.folder_manipulation import detect_folder
 from whisper_functions import generate_subtitles
 
-def action(language: str, directory: str, file_type):
+def action(language: str, directory: str, file_type, parallel: bool):
     mp3_paths = detect_folder(directory, file_type)
-    generate_subtitles(audio_files=mp3_paths, language=language,directory=directory)
+    generate_subtitles(audio_files=mp3_paths, language=language,directory=directory,parallel=parallel)
     
 
 class Handler(FileSystemEventHandler):
-    def __init__(self, language: str, directory: str, file_trigger: str, file_type: str):
+    def __init__(self, language: str, directory: str, file_trigger: str, file_type: str, parallel: bool):
         super().__init__()
         self.language = language
         self.directory = Path(directory)
         self.file_trigger = file_trigger
         self.file_type = file_type
+        self.parallel = parallel
 
     def on_created(self, event):
         path = Path(event.src_path)
         if path.suffix.lower() == self.file_trigger and path.is_file():
-            action(self.language, self.directory, self.file_type)
+            action(self.language, self.directory, self.file_type, self.parallel)
--- src/subtitles_generator/whisper_functions.py
+++ src/subtitles_generator/whisper_functions.py
@@ -2,32 +2,49 @@ import os
 
 from pathlib import Path
 from typing import List
-
 from faster_whisper import WhisperModel
+from concurrent.futures import ProcessPoolExecutor, as_completed
 from manipulation.subtitles_manipulation import style_subtitles 
 
 # try "large-v3-turbo", device="cuda" or device="metal", compute_type="float16"
 MODEL = WhisperModel("large-v3-turbo", device="cpu", compute_type="int8")
 
-def generate_subtitles(audio_files: List[Path], language: Path = "en", directory: str = "") -> None:
+def generate_subtitles(audio_files: List[Path], language: Path, directory: str, parallel: bool) -> None:
+
+
+    if parallel:
+        n_workers = len(audio_files)
+
+        with ProcessPoolExecutor(max_workers=n_workers) as ex:
+            futures = [ex.submit(_generate_subtitles, audio_file, language, directory) for audio_file in audio_files]
+
+            # Wait for all futures to complete
+            for future in as_completed(futures):
+                future.result()
     
-    for i, audio_file in enumerate(audio_files):
-        file_name = f"{directory}/{audio_file.name}mp3.srt"
-        print(f"Processing file {audio_file.name}\n\nFiles will be sent to {directory}\n\n")
+    else:
+        for audio_file in audio_files:
+            _generate_subtitles(audio_file, language, directory)
 
-        segments, info = MODEL.transcribe(str(audio_file), language=language)
+def _generate_subtitles(audio_file: Path, language: Path = "en", directory: str = "") -> None:
 
-        # Write SRT manually from faster_whisper segments
-        with open(file_name, "w", encoding="utf-8") as f:
-            for idx, segment in enumerate(segments):
-                # segment.start, segment.end, segment.text
-                start = _format_timestamp(segment.start)
-                end = _format_timestamp(segment.end)
-                text = segment.text.strip().replace("\n", " ")
-                f.write(f"{idx+1}\n{start} --> {end}\n{text}\n\n")
+    file_name = f"{directory}/{audio_file.name}mp3.srt"
+    print(f"Processing file {audio_file.name}\n\nFiles will be sent to {directory}\n\n")
 
-        subtitles_name, path = _get_subtitle_file_path_name(file_name)
-        style_subtitles(file=file_name, subtitle_name=subtitles_name, path=path)
+    segments, info = MODEL.transcribe(str(audio_file), language=language)
+
+    # Write SRT manually from faster_whisper segments
+    with open(file_name, "w", encoding="utf-8") as f:
+        for idx, segment in enumerate(segments):
+            # segment.start, segment.end, segment.text
+            start = _format_timestamp(segment.start)
+            end = _format_timestamp(segment.end)
+            text = segment.text.strip().replace("\n", " ")
+            f.write(f"{idx+1}\n{start} --> {end}\n{text}\n\n")
+
+    subtitles_name, path = _get_subtitle_file_path_name(file_name)
+    style_subtitles(file=file_name, subtitle_name=subtitles_name, path=path)
+
 def _format_timestamp(seconds: float) -> str:
     # Format seconds to SRT timestamp: HH:MM:SS,mmm
     hours = int(seconds // 3600)
